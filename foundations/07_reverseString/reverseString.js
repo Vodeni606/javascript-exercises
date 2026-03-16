4058205241d75782435ff9561f9f1eb95706d982
Vodeni606 <waterblast606@gmail.com>
2026-03-16 13:17:46 +0100

    Finished

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    let splitString = str.split('');
+    splitString.reverse();
+    return splitString.join('');
 };
 
 // Do not edit below this line
