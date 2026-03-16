23e099f5b195c0af2f6484bd62d7a09853f0236f
Vodeni606 <waterblast606@gmail.com>
2026-03-16 13:07:14 +0100

    finished

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+
+    let newstr = '';
+
+    if (num < 0){
+        return 'ERROR';
+    }
+
+    for( let i = 0; i < num; i++){
+
+        newstr = str + newstr;
+    }   
+    return newstr;
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
