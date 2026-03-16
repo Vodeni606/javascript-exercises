e41214a3536c085328b28a22080889aea21b755c
Vodeni606 <waterblast606@gmail.com>
2026-03-16 14:43:29 +0100

    Finished

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+
+         return array.filter(function(element){
+
+            return !args.includes(element);
+
+        })
+
+
+        
+
 };
+
+console.log(removeFromArray([1,2,3,4],3,2))
 
 // Do not edit below this line
 module.exports = removeFromArray;
