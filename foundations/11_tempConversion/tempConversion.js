e9ff3dcb6e3e96ecc7d1ff3cc5ed3c560f25cad7
Vodeni606 <waterblast606@gmail.com>
2026-03-17 11:27:54 +0100

    finished

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,29 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  let celsius = (Fahrenheit-32)*5/9;
+
+  if (celsius === 0){
+    return celsius;
+  }else{
+    let fixedCelsius = celsius.toFixed(1);
+    let floatNum = parseFloat(fixedCelsius);
+    return floatNum;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  let fahrenheit = (Celsius * 9/5 + 32)
+
+  if (fahrenheit === 0){
+    return fahrenheit;
+  }else {
+    let fixedFahrenheit = fahrenheit.toFixed(1);
+    let floatFahrenheit = parseFloat(fixedFahrenheit);
+    return floatFahrenheit;
+  }
+
 };
+
+
 
 // Do not edit below this line
 module.exports = {
