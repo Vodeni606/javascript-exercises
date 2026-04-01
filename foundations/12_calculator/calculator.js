358b4122ed7398e02c3f51780d803eec7cceaacc
Vodeni606 <waterblast606@gmail.com>
2026-04-01 13:59:09 +0200

    Finished

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,46 @@
-const add = function() {
-	
+const add = function(a,b) {
+	const sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	const sum = a - b;
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let total = 0;
+  for (let i = 0; i < numbers.length; i++){
+    total += numbers[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let total = 1;
+  for (let i = 0; i < numbers.length; i++){
+    total *= numbers[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base,exponent) {
+	let result = 1; 
+  for (let i = 0; i < exponent; i++){
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+  for (let i = a; i > 0; i--){
+    result *= i;
+  }
+  return result;
 };
+
+console.log(add(0,0));
 
 // Do not edit below this line
 module.exports = {
