f0eadd6dd39075bcaf33deed8d05e6e8785d7917
Vodeni606 <waterblast606@gmail.com>
2026-04-06 12:22:58 +0200

    finished

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let cleaned = word.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+    let reversed = cleaned.split('').reverse().join('');
+
+    return cleaned === reversed;
 };
+
+
+palindromes('A car, a man, a maraca.');
 
 // Do not edit below this line
 module.exports = palindromes;
