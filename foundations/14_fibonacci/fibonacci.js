37617fb21b08605d8bde94ed6abb41945ea4b6c5
Vodeni606 <waterblast606@gmail.com>
2026-04-06 12:42:35 +0200

    finished

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  n = parseInt(n); 
 
+  if(n === 1 || n === 2){
+    return 1;
+  }else if (n === 0){
+    return 0;
+  }else if (n < 0)
+    return "OOPS";
+
+  let prev1 = 0;  
+  let prev2 = 1;  
+  let current;  
+  
+  for (let i = 2; i <= n; i++) {
+    current = prev1 + prev2;  
+    prev1 = prev2
+    prev2 = current
+  }
+  
+  return current;
 };
 
 // Do not edit below this line
