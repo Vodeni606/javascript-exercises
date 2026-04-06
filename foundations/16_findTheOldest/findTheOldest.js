5bd40784d92d031403f5ceb7ecca51f21c523008
Vodeni606 <waterblast606@gmail.com>
2026-04-06 13:32:38 +0200

    finished

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+   return people.reduce((oldest, person) => {
+  
+  let currentAge = person.yearOfDeath 
+    ? person.yearOfDeath - person.yearOfBirth
+    : new Date().getFullYear() - person.yearOfBirth;
+  
+  let oldestAge = oldest.yearOfDeath
+    ? oldest.yearOfDeath - oldest.yearOfBirth
+    : new Date().getFullYear() - oldest.yearOfBirth;
+  
+  return currentAge > oldestAge ? person : oldest;
+}, people[0]);
 };
 
 // Do not edit below this line
